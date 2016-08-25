2724d240c3a4af23fa9076e185eaee6eee2eda12
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2016-08-25 15:28:02 +0430

    articles example

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var API = koaNeo4j.API;
 var app = koaNeo4jApp({
     cypher_directory: './cypher',
     apis: [
-        new API('GET', '/doctors', 'doctor_result.cyp'),
-        new API('POST', '/doctors', 'doctor_result.cyp', ['admin'], () => console.log('/doctors POST served.')),
-        new API('GET', '/doctor/:id', 'doctor_view.cyp')
+        new API('GET', '/articles', 'articles.cyp'),
+        new API('POST', '/articles', 'articles.cyp', ['admin'], () => console.log('/doctors POST served.')),
     ],
     database: {
         cypherDirectoryPath: './cypher/',
